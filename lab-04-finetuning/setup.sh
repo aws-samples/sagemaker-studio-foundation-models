db48bee98a7faa0a06bb069975ef2b9ff96f3372
Pranav Murthy <pranavvm@amazon.com>
2023-11-22 22:46:18 -0500

    Convert notebook to scripts to run in a Code Editor

---
+++ lab-04-finetuning/setup.sh
@@ -0,0 +1,6 @@
+sudo apt-get update 
+sudo apt-get install unzip
+pip install --upgrade pyfiglet  sagemaker datasets
+pip install ./sagemaker-2.297.1.dev0-py2.py3-none-any.whl
+unzip models.zip
+mv models ../
